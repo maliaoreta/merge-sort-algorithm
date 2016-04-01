5b48a8387769b9dd126db3b4c5a69678e973c0cc
maliaoreta <maliaoreta@gmail.com>
2016-03-31 18:10:27 -1000

    tests if mergeSort returns the sorted array

--- public/js/tests/merge-sort.spec.js
+++ public/js/tests/merge-sort.spec.js
@@ -4,4 +4,15 @@ describe('mergeSort', function () {
 
     expect(mergeSort).to.be.a('function');
   });
+
+  it('should return an Array', function () {
+
+    expect(mergeSort([1, 2, 3])).to.be.an('array');
+  });
+
+  it('should return the sorted Array', function () {
+
+    expect(mergeSort([3, 4, 2, 1])).to.deep.equal([1, 2, 3, 4]);
+    expect(mergeSort([9, 4, 3, 5, 10, 5, 0, 10])).to.deep.equal([0, 3, 4, 5, 5, 9, 10, 10]);
+  })
 });
